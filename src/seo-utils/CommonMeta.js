4091ab26dda6b8bbb012d0edd31984a6d41a6bde
MOHDSAKIB <saifishakib960@gmail.com>
2025-08-31 02:08:34 +0530

    Update CommonMeta.js

--- src/seo-utils/CommonMeta.js
+++ src/seo-utils/CommonMeta.js
@@ -5,10 +5,10 @@ export const createMetaData = ({
   title = `Artificial Mufti - Your AI Companion for Islamic Guidance`,
   description = `The Artificial Mufti is an AI-powered assistant providing thoughtful, humorous, and well-referenced guidance on Islamic topics. Blending traditional wisdom with modern technology, we make learning accessible and engaging.`,
   keywords = `Artificial Mufti, AI Mufti, Islamic guidance, ask a mufti, Islam, Muslim, Q&A, Quran, Hadith, Islamic AI, digital mufti, fatwa, Islamic knowledge`,
-  image = "https://www.artificial-mufti.vercel.app/assets/custom_icons/logo-full.png",
+  image = "https://artificial-mufti.vercel.app/assets/custom_icons/logo-full.png",
 } = {}) => {
   return {
-    metadataBase: new URL("https://www.artificial-mufti.vercel.app"),
+    metadataBase: new URL("https://artificial-mufti.vercel.app"),
     title,
     description,
     keywords,
@@ -22,7 +22,7 @@ export const createMetaData = ({
     },
     twitter: {
       card: "summary_large_image",
-      site: "https://www.artificial-mufti.vercel.app",
+      site: "https://artificial-mufti.vercel.app",
       title,
       description,
       images: [image],
